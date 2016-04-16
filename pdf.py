8f6502916ca04b56d6c630c6276fd7b09fdda24e
Jacques Supcik <jacques.supcik@hefr.ch>
2016-04-16 23:47:33 +0200

    WIP

---
+++ pdf.py
@@ -0,0 +1,127 @@
+# Copyright 2016 Jacques Supcik / Passeport Vacances Fribourg
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import subprocess
+import jinja2
+import os
+import re
+import tempfile
+import shutil
+import logging
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+LATEX_SUBS = (
+    (re.compile(r'\\'), r'\\textbackslash'),
+    (re.compile(r'([{}_#%&$])'), r'\\\1'),
+    (re.compile(r'~'), r'\~{}'),
+    (re.compile(r'\^'), r'\^{}'),
+    (re.compile(r'"'), r"''"),
+    (re.compile(r'\.\.\.+'), r'\\ldots{} '),
+    (re.compile(r'\x92'), r"'"),
+    (re.compile(r'\x85'), r'\ldots{} '),
+    (re.compile(r'\x96'), r'--'),
+)
+
+
+def escape_tex(value):
+    newval = value
+    for pattern, replacement in LATEX_SUBS:
+        newval = pattern.sub(replacement, newval)
+    return newval
+
+
+def cell_break(value):
+    value = re.sub(r'\n', r'\\newline{}', value)
+    return value
+
+
+def line_break(value):
+    value = re.sub(r'\n', r'\\\n', value)
+    return value
+
+
+def make(data, params, doc_src, main_tex, templates):
+    logger.debug("Building template")
+    cur_dir = os.getcwd()
+    logger.debug("Current working directory : {0}".format(cur_dir))
+    tmp_dir = tempfile.mkdtemp()
+    logger.debug("Temp directory : {0}".format(tmp_dir))
+    shutil.copytree(doc_src, os.path.join(tmp_dir, "tex"))
+    os.chdir(os.path.join(tmp_dir, "tex"))
+
+    texenv = jinja2.Environment(
+        extensions=['jinja2.ext.do'],
+        line_statement_prefix="#",
+        line_comment_prefix="##",
+        loader=jinja2.FileSystemLoader("."))
+    texenv.block_start_string = '((*'
+    texenv.block_end_string = '*))'
+    texenv.variable_start_string = '((('
+    texenv.variable_end_string = ')))'
+    texenv.comment_start_string = '((='
+    texenv.comment_end_string = '=))'
+    texenv.filters['escape_tex'] = escape_tex
+    texenv.filters['cell_break'] = cell_break
+    texenv.filters['line_break'] = line_break
+
+    logger.debug("Rendering")
+    for k, v in templates.items():
+        logger.debug("{0} -> {1}".format(k, v))
+        template = texenv.get_template(k)
+        f = open(v, mode="w", encoding="utf-8")
+        f.write(template.render(data=data, params=params))
+        f.close()
+
+    logger.debug("compiling latex")
+
+    cmd = "latexmk -silent -f -pdf -pdflatex='xelatex' {0}".format(main_tex)
+    logger.debug("> {0}".format(cmd))
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    try:
+        out, err = p.communicate(timeout=300)
+        logger.debug("return code: {0}".format(p.returncode))
+        logger.debug("----- BEGIN STDOUT -----")
+        logger.debug("\n" + out.decode("utf-8"))
+        logger.debug("----- END STDOUT / BEGIN STDERR -----")
+        logger.debug("\n" + err.decode("utf-8"))
+        logger.debug("----- END STDERR -----")
+    except Exception as e:
+        logger.error(type(e))
+        logger.error(e)
+        return None
+
+    cmd = "latexmk -silent -f -pdf -pdflatex='xelatex' -c {0}".format(main_tex)
+    logger.debug("> {0}".format(cmd))
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    try:
+        out, err = p.communicate(timeout=300)
+        logger.debug("return code: {0}".format(p.returncode))
+        logger.debug("----- BEGIN STDOUT -----")
+        logger.debug("\n" + out.decode("utf-8"))
+        logger.debug("----- END STDOUT / BEGIN STDERR -----")
+        logger.debug("\n" + err.decode("utf-8"))
+        logger.debug("----- END STDERR -----")
+    except Exception as e:
+        logger.error(type(e))
+        logger.error(e)
+        return None
+
+    res = open(os.path.splitext(main_tex)[0] + ".pdf", "rb").read()
+    os.chdir(cur_dir)
+
+    logger.debug("Cleaning up {0}".format(tmp_dir))
+    shutil.rmtree(tmp_dir)
+    return res
---
+++ telegram_bot.py
@@ -0,0 +1,112 @@
+from telegram.ext import Updater
+import logging
+import logging.handlers
+import groople
+import io
+import pymysql
+import argparse
+import pdf
+
+parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
+parser.add_argument("--bot-token", type=str, help="Bot secret Token")
+parser.add_argument("--db-host", type=str, help="Database Host")
+parser.add_argument("--db-user", type=str, help="Database Username")
+parser.add_argument("--db-password", type=str, help="Database Password")
+parser.add_argument("--db-database", type=str, help="Database Name")
+parser.add_argument("--debug", action="store_true", help="debug mode")
+
+args = parser.parse_args()
+
+logger = logging.getLogger(__name__)
+
+loggers = [
+    logger,
+    logging.getLogger('groople'),
+    logging.getLogger('pdf'),
+    logging.getLogger('telegram'),
+]
+
+log_formatter = logging.Formatter(
+    '%(asctime)s %(name)-10s %(levelname)-8s %(message)s')
+
+if args.debug:
+    handler = logging.StreamHandler()
+    handler.setFormatter(log_formatter)
+    for l in loggers:
+        l.addHandler(handler)
+        l.setLevel(logging.DEBUG)
+else:
+    handler = logging.handlers.RotatingFileHandler(
+        "/var/log/groople-pdf-maker.log",
+        mode='a', maxBytes=32 * 1024 * 1024, backupCount=5, encoding="UTF-8")
+    handler.setFormatter(log_formatter)
+    for l in loggers:
+        l.addHandler(handler)
+        l.setLevel(logging.INFO)
+
+
+def gen_pdf(one_page = False):
+    conn = pymysql.connect(
+        host=args.db_host,
+        user=args.db_user,
+        passwd=args.db_password,
+        db=args.db_database,
+        cursorclass=pymysql.cursors.DictCursor,
+    )
+
+    logger.debug("fetching data")
+    data = groople.Groople(conn).data()
+
+    params = {'onepage': 0}
+    if one_page:
+        params['onepage'] = 1
+
+    logger.debug("building PDF from LaTeX")
+    pdfs = pdf.make(
+        data, params,
+        doc_src="doc_src",
+        main_tex="pvf2016.tex",
+        templates={
+            "template.tex": "pvf2016.tex"})
+
+    return io.BufferedReader(io.BytesIO(pdfs))
+
+
+def start(bot, update):
+    bot.sendMessage(update.message.chat_id, text='Bonjour')
+
+
+def livret(bot, update):
+    logger.debug("Making livret")
+    bot.sendMessage(update.message.chat_id, text='Je prépare le livret... patience.')
+    fd = gen_pdf(one_page=False)
+    bot.sendDocument(update.message.chat_id, fd, filename="livret_2016.pdf")
+
+
+def bons_a_tirer(bot, update):
+    logger.debug("Making bons à tirer")
+    bot.sendMessage(update.message.chat_id, text='Je prépare les bons à tirer... patience.')
+    fd = gen_pdf(one_page=True)
+    bot.sendDocument(update.message.chat_id, fd, filename="bons_a_tirer_2016.pdf")
+
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+
+def main():
+    updater = Updater(args.bot_token)
+    dp = updater.dispatcher
+    dp.addTelegramCommandHandler("start", start)
+    dp.addTelegramCommandHandler("livret", livret)
+    dp.addTelegramCommandHandler("bons_a_tirer", bons_a_tirer)
+
+    dp.addErrorHandler(error)
+
+    logger.info("Bot ready")
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
